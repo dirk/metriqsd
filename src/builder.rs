47bcd79a4b2e307707c19027b7f6f9d6217eb521
Dirk Gadsden <dirk@esherido.com>
2018-01-17 10:59:09 -0500

    Start creatin builder interface for initializing sub-systems from configuration

---
+++ src/builder.rs
@@ -0,0 +1,58 @@
+use std::time::Duration;
+
+use metriqs::db::{Db, DbOptions};
+
+use super::config::{
+    Config,
+    Db as DbConfig,
+};
+
+macro_rules! option {
+    ($config:ident.$property:ident ?: $default:expr) => {
+        ($config.and_then(|config| config.$property)).unwrap_or_else(|| $default)
+    };
+    ($config:ident.$property:ident) => {
+        ($config.and_then(|config| config.$property))
+    }
+}
+
+trait Builder<T> {
+    fn build(&self) -> T;
+}
+
+pub struct RootBuilder {
+    db: DbBuilder,
+}
+
+impl From<Config> for RootBuilder {
+    fn from(config: Config) -> RootBuilder {
+        RootBuilder {
+            db: config.db.into(),
+        }
+    }
+}
+
+pub struct DbBuilder {
+    aggregation_interval: Option<Duration>,
+}
+
+impl From<Option<DbConfig>> for DbBuilder {
+    fn from(config: Option<DbConfig>) -> DbBuilder {
+        DbBuilder {
+            aggregation_interval: option!(config.aggregation_interval).map(f64_to_duration),
+        }
+    }
+}
+
+impl Builder<Db> for DbBuilder {
+    fn build(&self) -> Db {
+        let mut options: DbOptions = Default::default();
+        options.aggregation_interval = self.aggregation_interval;
+        Db::new(options)
+    }
+}
+
+fn f64_to_duration(value: f64) -> Duration {
+    let millis = (value * 1_000.0).round() as u64;
+    Duration::from_millis(millis)
+}
